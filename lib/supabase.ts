9baa2a7fe7b6285a638135b402535143094a5e88
zmh <zouminhua@jemincare.com>
2026-02-11 14:52:36 +0800

    移除supabase显式调用

--- lib/supabase.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
-
-export const supabaseServer = createClient(supabaseUrl, supabaseKey);
